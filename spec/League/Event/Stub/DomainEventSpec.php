636e5ad8059196f83bff84960f00faf41e24aa66
Frank de Jonge <info@frenky.net>
2014-06-26 17:59:29 +0200

    Tests for the EventAbstract::getName method

---
+++ spec/League/Event/Stub/DomainEventSpec.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace spec\League\Event\Stub;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class DomainEventSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('League\Event\Stub\DomainEvent');
+    }
+
+    function it_should_return_the_class_name_as_the_name()
+    {
+        $this->getName()->shouldBe('League\\Event\\Stub\\DomainEvent');
+    }
+}
---
+++ stubs/DomainEvent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace League\Event\Stub;
+
+use League\Event\EventAbstract;
+
+class DomainEvent extends EventAbstract
+{
+
+}
