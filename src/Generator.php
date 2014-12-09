8a332d1f38e7784cabc083afa5343fcf7ae29719
frankdejonge <info@frenky.net>
2014-12-09 22:00:37 +0100

    Removed duplicate inplementation of addEvent in the generator class, fixed in the trait use declaration.

--- src/Generator.php
+++ src/Generator.php
@@ -4,15 +4,7 @@ namespace League\Event;
 
 class Generator implements GeneratorInterface
 {
-    use GeneratorTrait;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function addEvent(EventInterface $event)
-    {
-        $this->events[] = $event;
-
-        return $this;
+    use GeneratorTrait {
+        addEvent as public;
     }
 }
