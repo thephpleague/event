e828632775f7b27a5df8b0da063700894636b439
Willem Mouwen <willem@mouwen.dev>
2025-01-10 13:39:12 +0100

    Add explicit nullable type for PHP 7.1 and higher

--- src/EmitterAwareInterface.php
+++ src/EmitterAwareInterface.php
@@ -2,21 +2,40 @@
 
 namespace League\Event;
 
-interface EmitterAwareInterface
-{
-    /**
-     * Set the Emitter.
-     *
-     * @param EmitterInterface $emitter
-     *
-     * @return $this
-     */
-    public function setEmitter(EmitterInterface $emitter = null);
+if (PHP_VERSION_ID < 70100) {
+    interface EmitterAwareInterface {
+        /**
+         * Set the Emitter.
+         *
+         * @param EmitterInterface $emitter
+         *
+         * @return $this
+         */
+        public function setEmitter(EmitterInterface $emitter = null);
 
-    /**
-     * Get the Emitter.
-     *
-     * @return EmitterInterface
-     */
-    public function getEmitter();
+        /**
+         * Get the Emitter.
+         *
+         * @return EmitterInterface
+         */
+        public function getEmitter();
+    }
+} else {
+    interface EmitterAwareInterface {
+        /**
+         * Set the Emitter.
+         *
+         * @param EmitterInterface $emitter
+         *
+         * @return $this
+         */
+        public function setEmitter(?EmitterInterface $emitter = null);
+
+        /**
+         * Get the Emitter.
+         *
+         * @return EmitterInterface
+         */
+        public function getEmitter();
+    }
 }
--- src/EmitterAwareTrait.php
+++ src/EmitterAwareTrait.php
@@ -2,40 +2,77 @@
 
 namespace League\Event;
 
-trait EmitterAwareTrait
-{
-    /**
-     * The emitter instance.
-     *
-     * @var EmitterInterface|null
-     */
-    protected $emitter;
+if (PHP_VERSION_ID < 70100) {
+    trait EmitterAwareTrait {
+        /**
+         * The emitter instance.
+         *
+         * @var EmitterInterface|null
+         */
+        protected $emitter;
 
-    /**
-     * Set the Emitter.
-     *
-     * @param EmitterInterface|null $emitter
-     *
-     * @return $this
-     */
-    public function setEmitter(EmitterInterface $emitter = null)
-    {
-        $this->emitter = $emitter;
+        /**
+         * Set the Emitter.
+         *
+         * @param EmitterInterface|null $emitter
+         *
+         * @return $this
+         */
+        public function setEmitter(EmitterInterface $emitter = null) {
+            $this->emitter = $emitter;
 
-        return $this;
-    }
-
-    /**
-     * Get the Emitter.
-     *
-     * @return EmitterInterface
-     */
-    public function getEmitter()
-    {
-        if (! $this->emitter) {
-            $this->emitter = new Emitter();
+            return $this;
         }
 
-        return $this->emitter;
+        /**
+         * Get the Emitter.
+         *
+         * @return EmitterInterface
+         */
+        public function getEmitter() {
+            if (!$this->emitter) {
+                $this->emitter = new Emitter();
+            }
+
+            return $this->emitter;
+        }
+    }
+} else {
+    trait EmitterAwareTrait
+    {
+        /**
+         * The emitter instance.
+         *
+         * @var EmitterInterface|null
+         */
+        protected $emitter;
+
+        /**
+         * Set the Emitter.
+         *
+         * @param EmitterInterface|null $emitter
+         *
+         * @return $this
+         */
+        public function setEmitter(?EmitterInterface $emitter = null)
+        {
+            $this->emitter = $emitter;
+
+            return $this;
+        }
+
+        /**
+         * Get the Emitter.
+         *
+         * @return EmitterInterface
+         */
+        public function getEmitter()
+        {
+            if (! $this->emitter) {
+                $this->emitter = new Emitter();
+            }
+
+            return $this->emitter;
+        }
     }
 }
